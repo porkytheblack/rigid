72188ad1edfbcbeac080f802b777c877ecad7412
porkytheblack <porkytheblack@gmail.com>
2026-02-05 01:57:27 +0300

    patch: rigid patch

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -515,7 +515,7 @@ export default async function HomePage() {
 
             <div className={styles.downloadGithub}>
               <a
-                href="https://github.com/porkytheblack/taka"
+                href="https://github.com/porkytheblack/rigid"
                 target="_blank"
                 rel="noopener noreferrer"
                 className={styles.githubBtn}
